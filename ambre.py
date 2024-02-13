2e2449bcd6802d29d40c935855924bc2b8f03c73
Dorian BERTRAND <56428513+DorianBertr@users.noreply.github.com>
2024-02-13 21:46:12 +0100

    Update ambre.py

--- ambre.py
+++ ambre.py
@@ -1,16 +1,16 @@
-import uuid, os, math, requests, time, datetime
+import discord, requests, time, datetime
 from bs4 import BeautifulSoup
-import discord
 from discord.ext import commands
 
-prefix="<"
-
+prefix="!"
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix=prefix, intents=intents, help_command=None)
 
 # Initialisation de l'id de l'admin
 admin=0
+# Stocke le moment du démarrage du bot
+start_time = time.time()
 
 
 # Événement de connexion
@@ -18,47 +18,133 @@ admin=0
 async def on_ready():
     nom=bot.user.name
     print(f'Connecté en tant que {nom}')
-    
     user = await bot.fetch_user(admin)
     # Envoie un message privé à l'utilisateur
-    await user.send(f"Bonjour Administrateur {user.mention}, je suis connecté en tant que {nom} !\nCode de sécurité : {code_secu}")
+    await user.send(f"Bonjour Administrateur {user.mention}, je suis connecté en tant que {nom} !")
 
 
-@bot.command(description="Obtenir la liste des scans sur le site WEB local.")
-async def scans(ctx):
+# Définition de la fonction verif
+async def verif(id):
+    if (id==admin):
+        return False
+    return True
+
+# Définition de la fonction send_dm
+async def annonce(member, serveur, action, valid, raison=None):
+    # Récupère l'utilisateur correspondant à l'ID spécifié
+    user = await bot.fetch_user(admin) 
+    message=""
+    opt=" "
+    opti=" "
+    optio=""
+    if action!="destroy":
+        opt=" te "
+        opti=" sur "
+        optio=f" pour la raison suivante : {raison}"
+    if valid==True:
+        message+=f"{member[0]} ({member[1]}) {action} le serveur {serveur}"
+    else:
+        message+=f"Tentative de{opt}{action}{opti}le serveur {serveur} par {member[0]} ({member[1]}){optio}"
+    # Envoie un message privé à l'admin    
+    await user.send(message)
+
+
+@bot.command(description="Liste les permissions de l'user mentionné. !perms @user")
+async def perms(ctx, member: discord.Member):
     # Supprime le message contenant la commande utilisée
     await ctx.message.delete()
-    # Chemin du dossier à parcourir
-    mangas_path = './mangas'
-    #Créé l'embed
-    embed = discord.Embed(title=f"{len(os.listdir(mangas_path))} scans disponibles", color=discord.Color.random())
-    
-    # Parcourt tous les dossiers dans le dossier spécifié
-    for manga in os.listdir(mangas_path):
-        # Chemin complet du dossier
-        manga_path = os.path.join(mangas_path, manga)
-        #Créé le message d'infos du manga
-        message=f"{len(os.listdir(manga_path))} chapitres.\n"
-        #Fait la moyenne
-        nb_img=0
-        for chapter in os.listdir(manga_path):
-            chapter_path=os.path.join(manga_path, chapter)
-            nb_img+=len(os.listdir(chapter_path))
-        moy_img=nb_img/len(os.listdir(manga_path)) 
-        message+=f"Moyenne de {math.floor(moy_img)} slides par chapitre."
-        embed.add_field(name=manga, value=message, inline=False)
+    # Crée une embed pour stocker les informations des permissions
+    embed = discord.Embed(title=f"Permissions de {member.name}", color = discord.Color.teal())
+    list_perms=[]
+    # Créer un objet Translator
+    # traducteur = Translator()
+    # Pour chaque rôle de l'utilisateur
+    for role in member.roles:
+        # Pour chaque permission du rôle
+        for perm, value in role.permissions:
+            if value is True:
+                if perm not in list_perms:
+                    list_perms.append(perm)
+                    new_perm = perm.replace("_", " ")
+                    # perm_trad = traducteur.translate(new_perm, dest='fr').text
+                    embed.add_field(name=new_perm, value="", inline=False)  
+    # Envoie les informations des permissions dans le canal où la commande a été utilisée
     await ctx.send(embed=embed)
 
+# Définition de la fonction action
+async def action(action, ctx, member, reason):
+    # Supprime le message contenant la commande utilisée
+    await ctx.message.delete()
+    message=""
+    fin=False
+    if (await verif(member.id)==True) :
+        # Vérifie si l'utilisateur a la permission de bannir des membres
+        autorisation=0
+        if action=="kick":
+            autorisation=ctx.author.guild_permissions.kick_members
+        else:
+            autorisation=ctx.author.guild_permissions.ban_members
+        if autorisation:
+            # Envoie un message privé à l'utilisateur
+            message=f"```ansi\n[2;31mVous avez été {action} du serveur {ctx.guild.name}"
+            if reason!=None:
+                message+=f" pour la raison suivante : {reason}"
+            message+="[0m```"
+            await member.send(message)
 
-@bot.command(description="Obtenir le status du site WEB local.")
-async def status(ctx):
+            message=f"> {member.name} a été {action} du serveur."
+            fin=True
+        else:
+            message=f"```ansi\n[2;31m[2;33mDésolé, vous n'avez pas la permission de {action} des membres.[0m[2;31m[0m```"
+    else :
+        message=f"```ansi\n[2;31m[2;33m{ctx.author.mention} tu ne peux pas {action} mon père.[0m[2;31m[0m```"
+        await annonce([ctx.author.name,ctx.author.id], ctx.guild.name, action, False, reason)
+    await ctx.send(message)
+    return fin
+
+@bot.command(description="Ban l'user mentionné. !ban @user [raison](optionnel)")
+async def ban(ctx, member: discord.Member, *, reason=None):
+    if await action("ban", ctx, member, reason):
+        await member.ban(reason=reason)
+
+@bot.command(description="Kick l'user mentionné. !kick @user [raison](optionnel)")
+async def kick(ctx, member: discord.Member, *, reason=None):
+    if await action("kick", ctx, member, reason):
+        await member.kick(reason=reason)
+        
+@bot.command(description="Fait parler le bot. !msg [message]")      
+async def msg(ctx, *, message):
+    # Supprime le message contenant la commande utilisée
+    await ctx.message.delete()
+    await ctx.send(message)
+
+@bot.command(description="Le bot mp l'user mentionné avec vottre message. !mp @user [message]")
+async def mp(ctx, member: discord.Member, *, message):
+    # Supprime le message contenant la commande utilisée
+    await ctx.message.delete()
+    # Envoie un message privé à l'utilisateur
+    await member.send(message)
+    
+@bot.command(description="Renomme l'user mentionné. !nick @user [surnom]")
+async def nick(ctx, member: discord.Member, *, nickname):
+    # Supprime le message contenant la commande utilisée
+    await ctx.message.delete()
+    # Change le surnom du membre
+    await member.edit(nick=nickname)
+
+@bot.command(description="Donne le status du site WEB local.")
+async def status(ctx, site=None):
     # Supprime le message contenant la commande utilisée
     await ctx.message.delete()
     # Création de l'embed pour afficher les informations
     embed = discord.Embed(title="Informations sur le site")
+    message=""
     try:
         # Effectuer une requête GET pour obtenir le contenu de la page
-        response = requests.get('http://localhost/')  
+        lien='lien du site'
+        if site!=None:
+            lien=site
+        response = requests.get(lien)  
         # Vérifier si la requête a réussi
         if response.status_code == 200:
             # Utiliser BeautifulSoup pour analyser le contenu HTML de la page
@@ -66,21 +152,20 @@ async def status(ctx):
             # Récupérer le titre de la page
             title = soup.title.string      
             # Afficher le titre de la page
-            embed.add_field(name="Connectivité", value=f"{title} est en ligne.", inline=False)
+            message=f"{title} est en ligne."
             embed.color = discord.Color.green()
         else:
-            embed.add_field(name="Connectivité", value=f"Impossible de récupérer le titre du site. Code de statut : {response.status_code}", inline=False)
+            message=f"Impossible de récupérer le titre du site. Code de statut : {response.status_code}"
             embed.color = discord.Color.orange()
     except requests.ConnectionError:
-        embed.add_field(name="Connectivité", value="Impossible de se connecter au site !", inline=False)
+        message="Impossible de se connecter au site !"
         embed.color = discord.Color.red()
-    embed.add_field(name="En savoir plus", value=f"{prefix}scans", inline=False)
+    embed.add_field(name=message, value="", inline=False)
+    if site==None:
+        embed.add_field(name="En savoir plus", value=f"{prefix}scans", inline=False)
     # Envoie l'embed dans le canal où la commande a été lancée
     await ctx.send(embed=embed)
 
-
-# Stocke le moment du démarrage du bot
-start_time = time.time()
 @bot.command(description="Obtenir les informations du bot.")
 async def infos(ctx):
     # Supprime le message contenant la commande utilisée
@@ -97,8 +182,7 @@ async def infos(ctx):
     # Envoie l'embed dans le canal où la commande a été lancée
     await ctx.send(embed=embed)
 
-
-@bot.command(description="Obtenir la liste des commandes utilisables.")
+@bot.command(description="Liste les commandes utilisables.")
 async def help(ctx):
     # Supprime le message contenant la commande utilisée
     await ctx.message.delete()
@@ -106,10 +190,10 @@ async def help(ctx):
     embed = discord.Embed(title="Commandes disponibles", color=discord.Color.purple())
     # Récupère la liste de toutes les commandes disponibles
     for command in bot.commands :
-        embed.add_field(name=command.name, value=command.description, inline=False)
+        embed.add_field(name=prefix+command.name, value=command.description, inline=False)
     # Envoie l'embed dans le canal où la commande a été lancée
     await ctx.send(embed=embed)
 
 
 # Exécution du bot avec le jeton Discord
-bot.run('token')
+bot.run('TOKEN')
