02da03c18272f4a53611beeacf2a69bcb79a0e81
Dorian BERTRAND <56428513+DorianBertr@users.noreply.github.com>
2024-03-14 19:15:56 +0100

    Update infos.py

--- infos.py
+++ infos.py
@@ -1,6 +1,6 @@
 def valeurs():
-    ssh_host = 'localhost'
-    bdd_username = 'dorian'
-    bdd_password = 'D0r!4n28'
-    bdd_name = 'course'
+    ssh_host = ''
+    bdd_username = ''
+    bdd_password = 'D0r!'
+    bdd_name = ''
     return [bdd_username, bdd_password, ssh_host, bdd_name]
