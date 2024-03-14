737ccf38a9906b2d9bec3b2e6f365942dd7937a9
Dorian BERTRAND <56428513+DorianBertr@users.noreply.github.com>
2024-03-14 19:14:38 +0100

    Delete infos.py

--- infos.py
+++
@@ -1,6 +0,0 @@
-def valeurs():
-    ssh_host = 'localhost'
-    bdd_username = 'dorian'
-    bdd_password = 'D0r!4n28'
-    bdd_name = 'course'
-    return [bdd_username, bdd_password, ssh_host, bdd_name]
